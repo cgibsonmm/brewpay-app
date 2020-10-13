b0903cfa87fff1de16e2d83e4d9ff647f234d0cb
corey gibson <cgibsonmm@gmail.com>
2020-10-13 16:29:21 -0400

    added button to homepage

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Home Page", type: :feature do
-  it"Should render" do
+
+  before do 
     visit '/'
+  end
+  it"Should render" do
     expect(page).to have_title('BrewPay')
     expect(page).to have_selector('h1', text: 'BrewPay')
+  end
+
+  it 'should have a Learn More Button' do
+    expect(page).to have_button('Learn More')
   end
 end
